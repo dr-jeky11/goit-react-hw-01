9788ab90f2ee4166da552ebd0005c98e98349b6c
dr-jeky11 <tyrsyna@gmail.com>
2024-06-29 22:40:42 +0200

    profile done

---
+++ src/components/App/App.jsx
@@ -0,0 +1,21 @@
+import userData from '../../userData.json'
+import friends from '../../friends.json'
+import transactions from '../../transactions.json'
+
+import Profile from '../Profile/Profile';
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
+};
+
+export default App;
