2522524189baeb6bf354eb051994e91568c8a041
dr-jeky11 <tyrsyna@gmail.com>
2024-06-29 23:18:47 +0200

    friend list, list item done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,8 @@ import friends from '../../friends.json'
 import transactions from '../../transactions.json'
 
 import Profile from '../Profile/Profile';
+import FriendList from '../FriendList/FriendList';
+import TransactionHistory from '../TransactionHistory/TransactionHistory';
 
 const App = () => {
   return (
@@ -14,6 +16,8 @@ const App = () => {
         image={userData.avatar}
         stats={userData.stats}
       />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
